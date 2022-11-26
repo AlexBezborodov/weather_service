b18419395b03527a284744846b5c643d0c1e056e
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-11-26 23:26:35 +0200

    Feature: add constants
    Signed-off-by: Alex Bezborodov <alexbezborodov23@gmail.com>

---
+++ src/constants/constants.ts
@@ -0,0 +1,3 @@
+export const API_KEY = "1db2eafd57c395fd6ee79ed8ab422f37";
+export const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?`;
+export const MULTIPLE_CITIES_URL = `http://api.openweathermap.org/data/2.5/group?id=`;
