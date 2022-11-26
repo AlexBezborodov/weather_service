1679c95f331654f132ae65cec492d309160c9a88
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-11-26 23:24:03 +0200

    Feature: add routes and update core components
    Signed-off-by: Alex Bezborodov <alexbezborodov23@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { CityDetails } from "./components/city_details";
+import { Header } from "./components/header";
+import "./App.css";
+import { Layout } from "./components/layout";
+import { MainPage } from "./components/main_page";
+import { store } from "./store/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/details/:city/:id" element={<CityDetails />} />
+            </Routes>
+          </Layout>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
