52357709f5835e8b224468bdb785fb0a1daca97f
Alex Bezborodov <alexbezborodov23@gmail.com>
2022-11-26 23:24:55 +0200

    Feature: add header component
    Signed-off-by: Alex Bezborodov <alexbezborodov23@gmail.com>

---
+++ src/components/header/header.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useMatch } from "react-router-dom";
+
+import Search from "../search/search";
+
+export const Header = () => {
+  const match = useMatch("/");
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "rgba(18, 18, 18, 0.345)" }}
+      >
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            OpenWeather
+          </Typography>
+          <Box sx={{ flexGrow: 1 }}></Box>
+          {match && <Search />}
+          <Box sx={{ flexGrow: 1 }}></Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
